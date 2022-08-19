6e7defd9264b66d4af18fbea9af9ea7e6c9ce656
bluesunkennie <67251928+bluesunkennie@users.noreply.github.com>
2022-08-19 23:38:58 +0700

    check_empty

--- src/view/device/AddDevice.js
+++ src/view/device/AddDevice.js
@@ -3,7 +3,11 @@ import { Typography } from "@mui/material";
 import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 import { db } from "../../firebase";
+import Alert from "@mui/material/Alert";
+import isEmpty from "validator/lib/isEmpty";
+import { useNavigate } from "react-router-dom";
 const AddDevice = () => {
+  const device = useNavigate();
   const [inputName, setInputName] = useState("");
   const [inputComment, setInputComment] = useState("");
   const [inputCpu, setInputCpu] = useState("");
@@ -15,6 +19,10 @@ const AddDevice = () => {
   const [inputSize, setInputSize] = useState("");
   const [inputVersion, setInputVersion] = useState("");
   const [inputProtection, setInputProtection] = useState("");
+  const [myAlert, setMyAlert] = useState(false);
+  const [myError, setMyError] = useState(false);
+  const [valid, setValid] = useState({});
+
   const handelValueName = (event) => {
     setInputName(event.target.value);
   };
@@ -48,13 +56,55 @@ const AddDevice = () => {
   const handelValueProtection = (event) => {
     setInputProtection(event.target.value);
   };
+
+  const validateAll = () => {
+    const msg = {};
+    if (isEmpty(inputName)) {
+      msg.name = "Please input  name device";
+    }
+    if (isEmpty(inputCpu)) {
+      msg.cpu = "Please input  cpu device";
+    }
+    if (isEmpty(inputIpAddress)) {
+      msg.ipAddress = "Please input ip address device";
+    }
+    if (isEmpty(inputVersion)) {
+      msg.version = "Please input version device";
+    }
+    if (isEmpty(inputSize)) {
+      msg.size = "Please input size device";
+    }
+    if (isEmpty(inputRam)) {
+      msg.ram = "Please input ram device";
+    }
+    if (isEmpty(inputService)) {
+      msg.service = "Please input service device";
+    }
+    if (isEmpty(inputLastBackup)) {
+      msg.lastBackup = "Please input last backup device";
+    }
+    if (isEmpty(inputComment)) {
+      msg.comment = "Please input comment device";
+    }
+    if (isEmpty(inputProtection)) {
+      msg.protection = "Please input protection device";
+    }
+    setValid(msg);
+    if (Object.keys(msg).length > 0) return false;
+    return true;
+  };
+
   const addDevice = async () => {
     // uploadSoundUsVocabulary();
     // await uploadSoundUkVocabulary();
+    const isValidation = validateAll();
+    if (!isValidation) return;
     axios
       .post(`http://192.168.1.7:8080/device`, { ...deviceAdd })
       .then((res) => {
-        window.location.reload();
+        setMyAlert(true);
+        // window.location.reload();
+        device("/");
       });
   };
   const deviceAdd = {
@@ -120,6 +170,16 @@ const AddDevice = () => {
                     height: "40px",
                   }}
                 ></input>
+                <Typography
+                  style={{
+                    color: "red",
+                    fontSize: "15px",
+                    paddingTop: "5px",
+                    marginLeft: "15px",
+                  }}
+                >
+                  {valid.name}
+                </Typography>
               </div>
               <div
                 style={{
@@ -140,6 +200,16 @@ const AddDevice = () => {
                     height: "40px",
                   }}
                 ></input>
+                <Typography
+                  style={{
+                    color: "red",
+                    fontSize: "15px",
+                    paddingTop: "5px",
+                    marginLeft: "15px",
+                  }}
+                >
+                  {valid.cpu}
+                </Typography>
               </div>
               <div
                 style={{
@@ -160,6 +230,16 @@ const AddDevice = () => {
                     height: "40px",
                   }}
                 ></input>
+                <Typography
+                  style={{
+                    color: "red",
+                    fontSize: "15px",
+                    paddingTop: "5px",
+                    marginLeft: "15px",
+                  }}
+                >
+                  {valid.ipAddress}
+                </Typography>
               </div>
               <div
                 style={{
@@ -180,6 +260,16 @@ const AddDevice = () => {
                     height: "40px",
                   }}
                 ></input>
+                <Typography
+                  style={{
+                    color: "red",
+                    fontSize: "15px",
+                    paddingTop: "5px",
+                    marginLeft: "15px",
+                  }}
+                >
+                  {valid.version}
+                </Typography>
               </div>
               <div
                 style={{
@@ -200,6 +290,16 @@ const AddDevice = () => {
                     height: "40px",
                   }}
                 ></input>
+                <Typography
+                  style={{
+                    color: "red",
+                    fontSize: "15px",
+                    paddingTop: "5px",
+                    marginLeft: "15px",
+                  }}
+                >
+                  {valid.size}
+                </Typography>
               </div>
             </div>
           </form>{" "}
@@ -232,7 +332,17 @@ const AddDevice = () => {
                     width: "90%",
                     height: "40px",
                   }}
-                ></input>
+                ></input>{" "}
+                <Typography
+                  style={{
+                    color: "red",
+                    fontSize: "15px",
+                    paddingTop: "5px",
+                    marginLeft: "15px",
+                  }}
+                >
+                  {valid.ram}
+                </Typography>
               </div>
               <div
                 style={{
@@ -253,6 +363,16 @@ const AddDevice = () => {
                     height: "40px",
                   }}
                 ></input>
+                <Typography
+                  style={{
+                    color: "red",
+                    fontSize: "15px",
+                    paddingTop: "5px",
+                    marginLeft: "15px",
+                  }}
+                >
+                  {valid.service}
+                </Typography>
               </div>
               <div
                 style={{
@@ -273,6 +393,16 @@ const AddDevice = () => {
                     height: "40px",
                   }}
                 ></input>
+                <Typography
+                  style={{
+                    color: "red",
+                    fontSize: "15px",
+                    paddingTop: "5px",
+                    marginLeft: "15px",
+                  }}
+                >
+                  {valid.lastBackup}
+                </Typography>
               </div>
               <div
                 style={{
@@ -293,6 +423,16 @@ const AddDevice = () => {
                     height: "40px",
                   }}
                 ></input>
+                <Typography
+                  style={{
+                    color: "red",
+                    fontSize: "15px",
+                    paddingTop: "5px",
+                    marginLeft: "15px",
+                  }}
+                >
+                  {valid.comment}
+                </Typography>
               </div>
               <div
                 style={{
@@ -313,6 +453,16 @@ const AddDevice = () => {
                     height: "40px",
                   }}
                 ></input>
+                <Typography
+                  style={{
+                    color: "red",
+                    fontSize: "15px",
+                    paddingTop: "5px",
+                    marginLeft: "15px",
+                  }}
+                >
+                  {valid.protection}
+                </Typography>
               </div>
             </div>
           </form>{" "}
@@ -339,6 +489,8 @@ const AddDevice = () => {
         >
           Add device
         </button>
+        {myAlert && <Alert severity="success">Add device successfully</Alert>}
+        {myError && <Alert severity="error">Can't add device!</Alert>}
       </div>
     </>
   );
